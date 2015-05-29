14ff3a415bb21183551734a5f83ca69fdb86e3f2
artemmmon <thumanazar@yandex.ru>
2015-05-29 21:32:44 +0300

    responsive style for secton 2

--- src/webapp/resources/js/fullPage.js
+++ src/webapp/resources/js/fullPage.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+	if ($(document).width() <= 768) { 
+		tf = true;
+	}
+	else {
+		tf = false;
+	}	
     $('#fullpage').fullpage({
     	anchors:['fisrtPage','secondPage','3Page', '4Page', '5Page', '6Page'],
-    	// menu:'#menu',
+    	// normalScrollElements: '#section1',
+    	scrollOverflow: tf,
     });
 });
