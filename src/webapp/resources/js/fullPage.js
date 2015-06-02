b0a89e0c3a277b8e821cf49492c9c07160d8917d
artemmmon <thumanazar@yandex.ru>
2015-06-02 22:34:17 +0300

    full hd style

--- src/webapp/resources/js/fullPage.js
+++ src/webapp/resources/js/fullPage.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 	else {
 		tf = false;
 	}	
-    $('#fullpage').fullpage({
-    	anchors:['fisrtPage','secondPage','3Page', '4Page', 'fifthPage', 'sixPage'],
-    	// normalScrollElements: '#section1',
-    	scrollOverflow: tf
-    });
+    // $('#fullpage').fullpage({
+    // 	anchors:['fisrtPage','secondPage','3Page', '4Page', 'fifthPage', 'sixPage'],
+    // 	// normalScrollElements: '#section1',
+    // 	scrollOverflow: tf
+    // });
 });
