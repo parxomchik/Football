7336dd4a3110f38c306808878a81146665497163
parkhomenko <dmitriy.parkhomenko@icloud.com>
2015-06-09 16:23:22 +0300

    add mainCtrl and feedback json

--- src/webapp/resources/admin/js/app.js
+++ src/webapp/resources/admin/js/app.js
@@ -4,23 +4,6 @@ var app = angular.module('mgcrea.ngStrapDocs', ['ngAnimate', 'ngSanitize', 'mgcr
 
 angular.module('mgcrea.ngStrapDocs');
 
-//app.controller('DatepickerDemoCtrl', function($scope) {
-//    $scope.selectedDate = new Date();
-//    $scope.getType = function(key) {
-//        return Object.prototype.toString.call($scope[key]);
-//    };
-//
-//    $scope.clearDates = function() {
-//        $scope.selectedDate = null;
-//    };
-//});
-//app.config(function($datepickerProvider) {
-//    angular.extend($datepickerProvider.defaults, {
-//        dateFormat: 'dd.MM.yyyy',
-//        startWeek: 1
-//    });
-//})
-
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
@@ -31,20 +14,36 @@ app.config(function($routeProvider) {
             templateUrl: 'client_page.html',
             controller: 'clientpageCtrl'
         })
-        .when('/features', {
-            templateUrl: 'client_page.html',
-            controller: 'clientpageCtrl'
-        })
-        .when('/prices', {
-            templateUrl: 'client_page.html',
-            controller: 'clientpageCtrl'
-        })
         .otherwise({
             redirectTo: '/404'
         });
     //$locationProvider.html5Mode(true);
 });
+app.controller("mainCtrl", function($scope,$http, $alert) {
+        $scope.feedback_submit = function(){
 
+        var feedback_info = {name:$scope.feedback_name, email:$scope.feedback_email, subject:feedback_subject, message:feedback_message};
+        console.log(feedback_info);
+//        window.location.replace("#/clientpage");
+        $http.post("http://127.0.0.1/rest/feedbacks",feedback_info)
+            .success(function (data) {
+//                if (data !== ""){
+//                    window.location.replace("#/clientpage");
+//                    $rootScope.slugebkis = JSON.parse(data)
+//                    $rootScope.userData = {Id:$scope.user_id, Pass:$scope.user_pass };
+            console.log(data)
+                }
+                else{
+//                    parol.show();
+                     alert("error");
+                }
+
+            })
+            .error(function (data){
+                console.log(data)
+            });
+    }
+});
 app.controller("loginCtrl", function($scope,$http, $alert, $rootScope) {
     $rootScope.name = {};
     var parol = $alert({
@@ -56,16 +55,6 @@ app.controller("loginCtrl", function($scope,$http, $alert, $rootScope) {
         duration : '3',
         show: false
     });
-    $scope.logins = {}
-//    $http.post("http://10.7.131.134/exampleService/PostWithZero/")
-//        .success(function (data) {
-//
-//            $scope.logins = JSON.parse(data);
-//        })
-//        .error(function (data)
-//        {
-//            $scope.error = "SUBMIT ERROR";
-//        });
     $scope.login_submit = function user_authorization(){
 
         var user_info = {name:$scope.user_name, Pass:$scope.user_pass };
