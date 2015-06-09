fd49a33db066b76468ed0014383cec22b457257d
Vladislav Kopylash <vladik.kopilash@gmail.com>
2015-06-09 15:14:57 +0300

    added fields to entities News and Feedback

--- dbFill.sql
+++ dbFill.sql
@@ -1,4 +1,4 @@
-insert into news values (1,'News 1','Shord description 1', 'Shot mazafaka', LOAD_FILE('c:/Downloads/2.jpg'), NOW());
+insert into news values (1,'Taras Plavin','Football','Championship, IT, startup','websolutions, football3x3','News 1','Shord description 1', 'Shot mazafaka', LOAD_FILE('c:/Downloads/2.jpg'), NOW());
 
 insert into teams values (1,'INVESTOR', 'BU', 'bu.com', 'Zermon','+380672693543', 'bu@gmail.com', load_file('c:/Downloads/mops.jpg'));
 insert into players(name,surname,role,captain,team_id) values ('Max', 'Mad', 'forward',true,1);
