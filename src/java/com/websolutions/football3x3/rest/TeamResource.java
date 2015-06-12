3e549954b2c6bab32889633ba132527b530de0de
Vladislav Kopylash <vladik.kopilash@gmail.com>
2015-06-12 20:29:55 +0300

    added UNIQUE constraint to team name

--- src/java/com/websolutions/football3x3/rest/TeamResource.java
+++ src/java/com/websolutions/football3x3/rest/TeamResource.java
@@ -9,8 +9,10 @@ import com.websolutions.football3x3.util.EmailService;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import javax.persistence.PersistenceException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
@@ -66,15 +68,30 @@ public class TeamResource {
         return team.getPlayers();
     }
 
+    /**
+     * Creates new Team and persists it to DB.
+     *@throws WebApplicationException with status code 403 - Forbidden
+     * @param team new entry
+     * @return merged Team entity
+     *
+     */
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Team createTeam(Team team) {
-        Team t = teamDao.save(team);
-        for (Player p : team.getPlayers()) {
-            p.setTeam(t);
-            playerDao.save(p);
+        Team t = null;
+        try {
+            t = teamDao.save(team);
+            for (Player p : team.getPlayers()) {
+                p.setTeam(t);
+                playerDao.save(p);
+            }
+        } catch (PersistenceException e) {
+            throw new WebApplicationException(e,403);
         }
+
+
         Team resultTeam = teamDao.find(t.getId());
         emailService.sendNotificationAboutNewTeam(resultTeam, "vladik.kopilash@gmail.com");
         return resultTeam;
--- src/java/com/websolutions/football3x3/util/CharsetResponseFilter.java
+++ src/java/com/websolutions/football3x3/util/CharsetResponseFilter.java
@@ -11,10 +11,13 @@ import javax.ws.rs.core.MediaType;
 
 public class CharsetResponseFilter implements ContainerResponseFilter {
 
+    @Override
     public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
 
         MediaType contentType = response.getMediaType();
-        response.getHttpHeaders().putSingle("Content-Type", contentType.toString() + ";charset=UTF-8");
+        if (contentType!=null) {
+            response.getHttpHeaders().putSingle("Content-Type", contentType.toString() + ";charset=UTF-8");
+        }
 
         return response;
     }
