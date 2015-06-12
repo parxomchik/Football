c8013de06d4564cd59bca4ab773ae6a513ec6661
artemmmon <thumanazar@yandex.ru>
2015-06-12 20:48:11 +0300

    fix video background, fix links

--- src/webapp/resources/js/video_background.js
+++ src/webapp/resources/js/video_background.js
@@ -1,7 +1,31 @@
+// $( document ).ready(function() {
+
 window_w = document.body.clientWidth;
+
+bg_height = $('#bg').innerHeight();
+
+$('#bg').css('height', bg_height + 90 +'px');
+
 if(window_w >= '992'){
-	$('#section').vide('./resources/video/background.mp4');
+	setTimeout("$('#section').delay(300).css({'background': 'url(./resources/images/section1.jpg) no-repeat', 'background-size': 'cover'})", 1000);
 }
 else {
-	$('#section').addClass('bg');
+	$('#section').delay(300).css({'background': 'url(./resources/images/section1.jpg) no-repeat', 'background-size': 'cover'});
 }
+function bg_hide() {
+	$('#bg').fadeOut().delay(10000);
+	bg_show();
+}
+function bg_show() {
+	$('#bg').fadeIn().delay(6000);
+	bg_hide();
+}
+function bg() {
+	$('#bg').vide('./resources/video/background.mp4');
+	bg_show();
+}
+bg();
+
+
+
+// });
