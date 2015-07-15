ef3f25313ecd02a5004240ffe1af27f9294f79b4
artemmmon <thumanazar@yandex.ru>
2015-07-15 21:56:55 +0300

    Balalayka - comeback 2!

--- src/webapp/resources/js/video_background.js
+++ src/webapp/resources/js/video_background.js
@@ -1,34 +1,34 @@
-
-
+$(document).ready(function() {
+	bg();
+})
 window_w = document.body.clientWidth;
 
 bg_height = $('#bg').innerHeight();
-
 $('#bg').css('height', bg_height + 90 +'px');
 
 if(window_w >= '992'){
-	setTimeout("$('#section').delay(300).css({'background': 'url(./resources/images/section1.jpg) no-repeat', 'background-size': 'cover'})", 1000);
+	setTimeout("$('#section').css({'background': 'url(./resources/images/section1.jpg) no-repeat', 'background-size': 'cover'})", 1000);
+	$('#section').css({'background': 'url(./resources/images/section1.jpg) no-repeat', 'background-size': 'cover'});
 }
 else {
-	$('#section').delay(300).css({'background': 'url(./resources/images/section1.jpg) no-repeat', 'background-size': 'cover'});
+	$('#bg').css({'background': 'url(./resources/images/section1.jpg) no-repeat', 'background-size': 'cover'});
 }
 
 function bg_hide() {
-	$('#bg').fadeOut("slow");
-	// bg_show();
+	// $('#bg').fadeOut("slow");
+	// $('#bg').fadeIn('slow');
 }
 function bg_show() {
 	$('#bg').fadeIn("slow").delay(8500);
 	bg_hide();
 }
 function bg() {
-	// $('#bg').vide('./resources/video/background.mp4');
 	bg_show();
 }
-// $( document ).ready(function() {
-setTimeout("bg()", 2800);
+
 
 // });
 $('#bg').vide({
   mp4: './resources/video/background.mp4'
 });
+// $('#bg').tubular({videoId: 'ixKK2Lon-Q0', repeat: true});
